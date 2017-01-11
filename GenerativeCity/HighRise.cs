7e5c138bdb7448fabeb51dcb2268f66267a4aa7e
Theino <theino8@yahoo.com>
2017-01-10 19:16:04 -0500

    Added structure of HighRise class

---
+++ GenerativeCity/HighRise.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenerativeCity
+{
+    class HighRise : CityStructure
+    {
+        public override double DefaultRandomChancePromotion
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        public override double DefaultRandomChanceDemotion
+        {
+            get
+            {
+                return 0.1;
+            }
+        }
+
+        public override double DefaultPromotionMultiplierOnUpgradeType
+        {
+            get
+            {
+                return 0.2;
+            }
+        }
+
+        public override Type promotionType
+        {
+            get
+            {
+                return typeof(CommercialBuilding);
+            }
+        }
+
+        public override Type demotionType
+        {
+            get
+            {
+                return typeof(Empty);
+            }
+        }
+
+        public override double DefaultCenterBias
+        {
+            get
+            {
+                return 0.1;
+            }
+        }
+
+        public HighRise(int xIndex, int yIndex, double randomChancePromotion, double randomChanceDemotion, double promotionMultiplierOnUpgradeType, double centerBias) : base(xIndex, yIndex, randomChancePromotion, randomChanceDemotion, promotionMultiplierOnUpgradeType, centerBias)
+        {
+        }
+
+        public HighRise(int xIndex, int yIndex) : base(xIndex, yIndex)
+        {
+            RandomChancePromotion = DefaultRandomChancePromotion;
+            RandomChanceDemotion = DefaultRandomChanceDemotion;
+            PromotionMultiplierOnUpgradeType = DefaultPromotionMultiplierOnUpgradeType;
+        }
+
+        public override void Step(CityMap cityMap)
+        {
+            object[] parameters = { XIndex, YIndex };
+            int xMiddle = cityMap.XSize / 2;
+            int yMiddle = cityMap.YSize / 2;
+            int randVal = cityMap.rand.Next();
+            double randomChanceWithHouse = cityMap.CountPercentTypeAround(XIndex, YIndex, 4, typeof(House));
+            double randomChanceWithCommercialBuildingFar = cityMap.CountPercentTypeAround(XIndex, YIndex, 4, typeof(CommercialBuilding));
+            double randomChanceWithCommercialBuildingNear = cityMap.CountPercentTypeAround(XIndex, YIndex, 1, typeof(CommercialBuilding));
+            double randomChanceWithSurroundingBias;
+            if (randomChanceWithCommercialBuildingFar > 0.1)
+            {
+                randomChanceWithSurroundingBias = 0;
+            }
+            else
+            {
+                randomChanceWithSurroundingBias = 3 * randomChanceWithCommercialBuildingNear + randomChanceWithHouse;
+            }
+
+            double percentCenterBias = calculateCenterBias(xMiddle, yMiddle);
+            double randomOddsOfPromotion = (randomChanceWithSurroundingBias * PromotionMultiplierOnUpgradeType) + RandomChancePromotion;
+            randomOddsOfPromotion = randomOddsOfPromotion * percentCenterBias;
+            if (randVal < Int32.MaxValue * randomOddsOfPromotion)
+            {
+                CityStructure newStructure = (CityStructure)Activator.CreateInstance(promotionType, parameters);
+                cityMap[XIndex, YIndex] = newStructure;
+            }
+        }
+    }
+}
